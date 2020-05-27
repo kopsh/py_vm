75f602795a8a7f747d32a2a590111955b0ff2bbe
宋昊 <kopbupt@126.com>
2020-05-27 23:41:27 +0800

    support if

--- code/byteCode.hpp
+++ code/byteCode.hpp
@@ -5,5 +5,23 @@ public:
     static const unsigned char PRINT_NEWLINE = 72;
     static const unsigned char RETURN_VALUE = 83;
     static const unsigned char HAVE_ARGUMENT = 90;
+
+    /* index in const list */
     static const unsigned char LOAD_CONST = 100;
+
+    /* compare operator */
+    static const unsigned char COMPARE_OF = 107;
+
+    /* number of bytes to skip */
+    static const unsigned char JUMP_FORWARD = 110;
+    static const unsigned char POP_JUMP_IF_FALSE = 114;
+
+    enum COMPARE {
+        LESS = 0,
+        LESS_EQUAL,
+        EQUAL,
+        NOT_EQUAL,
+        GREATER,
+        EQUALER_GREATER
+    }
 };
