3b268ab0505444c09bf5f1167e8cb9b1fdeb3613
宋昊 <kopbupt@126.com>
2020-06-24 18:34:29 +0800

    setdefault

--- object/HiDict.cpp
+++ object/HiDict.cpp
@@ -1,4 +1,6 @@
 # include "object/HiDict.hpp"
+# include "object/HiString.hpp"
+# include "runtime/functionObject.hpp"
 # include <iostream>
 
 DictKlass* DictKlass::instance = NULL;
@@ -10,7 +12,13 @@ DictKlass* DictKlass::get_instance() {
 }
 
 DictKlass::DictKlass() {
-    
+
+}
+
+void DictKlass::initialize() {
+    HiDict* klass_dict = new HiDict();
+    klass_dict->put(new HiString("setdefault"), new FunctionObject(set_dict_default));
+    set_klass_dict(klass_dict);
 }
 
 HiDict::HiDict() {
@@ -21,4 +29,19 @@ HiDict::HiDict() {
 HiDict::HiDict(ObjMap map) {
     _map = map;
     set_klass(DictKlass::get_instance());
+}
+
+HiObject* set_dict_default(ObjList args) {
+    HiDict* dict = (HiDict*) args->get(0);
+    HiObject* key = args->get(1);
+    HiObject* value = args->get(2);
+
+    if (!dict->has_key(key)) {
+        dict->put(key, value);
+    }
+    else {
+        return dict->get(key);
+    }
+
+    return value;
 }
--- object/HiDict.hpp
+++ object/HiDict.hpp
@@ -3,16 +3,17 @@
 
 # include "object/HiObject.hpp"
 # include "object/Klass.hpp"
+# include "util/ArrayList.hpp"
 # include "util/Map.hpp"
 
 class DictKlass : public Klass {
 private:
     DictKlass();
     static DictKlass* instance;
-    void initialize();
 
 public:
     static DictKlass* get_instance();
+    void initialize();
 
     // virtual HiObject* subscr(HiObject* x, HiObject* y);
     // virtual HiObject* iter(HiObject* x);
@@ -37,5 +38,7 @@ public:
     bool has_key(HiObject* k) { return _map->has_key(k); }
     int size() { return _map->size(); }
 };
+
+HiObject* set_dict_default(ObjList args);
 
 # endif
